2f0f51214e5137855ced981ca2b36f572034f176
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2023-01-26 15:13:03 +0800

    refact(extract): specify parameters using argparse

--- extract/extract.py
+++ extract/extract.py
@@ -19,60 +19,6 @@ from tqdm import tqdm
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def calculatePerplexity(sentence, model, tokenizer):
-    """
-    exp(loss)
-    """
-    input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
-    input_ids = input_ids.to(device)
-    with torch.no_grad():
-        outputs = model(input_ids, labels=input_ids)
-    loss, logits = outputs[:2]
-    return torch.exp(loss)
-
-def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=10):
-    """
-    print the `n` best samples according to the given `metric`
-    """
-    idxs = np.argsort(metric)[::-1][:n]
-
-    for i, idx in enumerate(idxs):
-        if scores2 is not None:
-            print(f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}")
-        else:
-            print(f"{i+1}: {name1}={scores1[idx]:.3f}, , score={metric[idx]:.3f}")
-
-        print()
-        #for line in samples[idx].split("\n"):
-        #    print(f"\t {line.rstrip()}")
-        pprint(samples[idx])
-        print()
-        print()
-        
-
-def parse_commoncrawl(wet_file):
-    """
-    Quick and ugly parsing of a WET file.
-    Tested for the May 2021 crawl.
-    """
-    with open(wet_file) as f:
-        lines = f.readlines() 
-    
-    start_idxs = [i for i in range(len(lines)) if "WARC/1.0" in lines[i]]
-    
-    all_eng = ""
-
-    count_eng = 0
-    for i in range(len(start_idxs)-1):
-        start = start_idxs[i]
-        end = start_idxs[i+1]
-        if "WARC-Identified-Content-Language: eng" in lines[start+7]:
-            count_eng += 1
-            for j in range(start+10, end):
-                all_eng += lines[j]
-
-    return all_eng
-
 
 def get_model_and_tokenizer(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -95,14 +41,11 @@ def main():
     path_to_save = 'results/{}'.format(model_name)
     os.makedirs(path_to_save, exist_ok=True)
     
-    
-    print(f"using device: {device}")
 
     # number of tokens to generate
-    seq_len = 256
-
+    seq_len = args.seq_len
     # sample from the top_k tokens output by the model
-    top_k = 40
+    top_k = args.top_k
 
     
     tokenizer, model = get_model_and_tokenizer(model_name)
@@ -147,6 +90,8 @@ def parse_arguments(argv):
     parser.add_argument('--N', type=int, default=20000, help="Number of samples to generate")
     parser.add_argument('--batch-size', type=int, default=20, help="Batch size for generation")
     parser.add_argument('--temperature', type=float, default=1.0, help="Start temperature")
+    parser.add_argument('--seq_len', type=int, default=256, help="The length of extracted sequence")
+    parser.add_argument('--top_k', type=int, default=256, help="sample from the top_k tokens output by the model")
 
     return parser.parse_args(argv)
 
