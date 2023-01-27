4027ae3350077a61d2a08f28eb4f3a0e77d4e15d
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2023-01-27 15:27:03 +0800

    feat(extract): get additional inforamtion, e.g., md5

--- extract/merge.py
+++ extract/merge.py
@@ -3,6 +3,10 @@ import os
 import argparse
 import logging
 from tqdm import tqdm
+import hashlib
+import json
+
+
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
@@ -26,10 +30,29 @@ if __name__ == '__main__':
     logger.info("Found {} files".format(len(files)))
 
     # merge all the files into one
+    curser = 0
+    map = {}
     with open(os.path.join(result_path, 'all'), 'w') as f:
-        for file in tqdm(files):
+        for file in tqdm(range(len(files))):
+            file = str(file + 1)
+            # jsut to make sure that the file is stored in the right order
             with open(os.path.join(seperate_path, file), 'r') as f2:
-                f.write(f2.read())
+                content = f2.read()
+                # compute MD5
+                md5 = hashlib.md5(content.encode('utf-8')).hexdigest()
+                to_write = content + '\nSubmission>>>>>>' + md5 + '>>>>>>Submission\n'
+                num_of_line = len(to_write.split('\n'))
+                f.write(to_write)
+
+                # store information to map
+                map[file] = {'md5': md5, 'start': curser + 1, 'end': curser + num_of_line - 1}
+
+                # update curser
+                curser = curser + num_of_line - 1
+
+    # store map into json file
+    with open(os.path.join(result_path, 'map.json'), 'w') as f:
+        json.dump(map, f)
 
     logger.info("Merged file is saved to {}".format(os.path.join(result_path, 'all')))
 
