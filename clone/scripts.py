63b19ba35a59d64b021996063bfe752780e604b3
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2023-02-20 10:37:04 +0800

    feat(clone): redirect output to log files

--- clone/scripts.py
+++ clone/scripts.py
@@ -20,19 +20,32 @@ if __name__ == '__main__':
     log_dir = os.path.join(root_dir, 'log/save/', generated_folder, str(size))
     os.makedirs(log_dir, exist_ok=True)
 
+    # build the commands to run
     commands = []
     for data in os.listdir(data_dir):
         data_path = os.path.join(data_dir, data)
         log_path = os.path.join(log_dir, data+".log")
         generated_path = os.path.join(root_dir, 'extract/results/{}/all_{}'.format(generated_folder, size))
         assert os.path.exists(generated_path)
-        commands.append(['java', '-jar', tool_path, data_path, generated_path, '>', os.path.join(log_dir, data+".log")])
+        commands.append(['java', '-jar', tool_path, data_path, generated_path])
+        # commands.append(['java', '-jar', tool_path, '/mnt/hdd1/zyang/Privacy-in-Code-Models/extract/utils.py'])
         print(commands[-1])
     
+    # launch the commands in parallel using a process pool
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = [executor.submit(run_command, command) for command in commands]
 
-
+    # wait for all futures to complete and capture the output
     outputs = [future.result() for future in futures]
+
+    # decode the output and error into strings (assuming utf-8 encoding)
+    output_strs = [output.decode("utf-8") for output, error in outputs]
+    error_strs = [error.decode("utf-8") for output, error in outputs]
+
+    # save output to files
+    for i, output_str in enumerate(output_strs):
+        with open(os.path.join(log_dir, "{}.log".format(i)), "w") as f:
+            f.write(output_str)
+
 
     print("Java command finished")
