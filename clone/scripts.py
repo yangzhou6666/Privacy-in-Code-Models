9f686d20de2854ec9377c2c9266c5c9cab7e19f9
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2023-02-20 11:05:09 +0800

    feat(clone): run each batch in sequence

--- clone/scripts.py
+++ clone/scripts.py
@@ -16,36 +16,45 @@ if __name__ == '__main__':
     data_dir = os.path.join(root_dir, 'clone/save/codeparrot/codeparrot-clean')
     tool_path = os.path.join(root_dir, 'clone/simian-2.5.10.jar')
 
-    size = 10000
-    log_dir = os.path.join(root_dir, 'log/save/', generated_folder, str(size))
-    os.makedirs(log_dir, exist_ok=True)
-
-    # build the commands to run
-    commands = []
-    for data in os.listdir(data_dir):
-        data_path = os.path.join(data_dir, data)
-        log_path = os.path.join(log_dir, data+".log")
-        generated_path = os.path.join(root_dir, 'extract/results/{}/all_{}'.format(generated_folder, size))
-        assert os.path.exists(generated_path)
-        commands.append(['java', '-jar', tool_path, data_path, generated_path])
-        # commands.append(['java', '-jar', tool_path, '/mnt/hdd1/zyang/Privacy-in-Code-Models/extract/utils.py'])
-        print(commands[-1])
-    
-    # launch the commands in parallel using a process pool
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(run_command, command) for command in commands]
-
-    # wait for all futures to complete and capture the output
-    outputs = [future.result() for future in futures]
-
-    # decode the output and error into strings (assuming utf-8 encoding)
-    output_strs = [output.decode("utf-8") for output, error in outputs]
-    error_strs = [error.decode("utf-8") for output, error in outputs]
-
-    # save output to files
-    for i, output_str in enumerate(output_strs):
-        with open(os.path.join(log_dir, "{}.log".format(i)), "w") as f:
-            f.write(output_str)
 
 
-    print("Java command finished")
+    step = 2 # 2 * 100000
+    start = 0
+    end = 20 # 20 * 100000
+    # process in chunks
+    for size in range(start, end, step):
+        _start = size * 100000
+        _end = (size + step) * 100000
+        file_path = os.path.join(root_dir, 'extract/results/{}/all_{}-{}'.format(generated_folder, _start, _end))
+        assert os.path.exists(file_path), "File {} does not exist".format(file_path)
+
+        # create log dir
+        log_dir = os.path.join(root_dir, 'log/save/', generated_folder, "{}-{}".format(_start, _end))
+        os.makedirs(log_dir, exist_ok=True)
+
+        # build the commands to run
+        commands = []
+        for data in os.listdir(data_dir):
+            data_path = os.path.join(data_dir, data)
+            commands.append(['java', '-jar', tool_path, file_path])
+            # commands.append(['java', '-jar', tool_path, data_path, generated_path])
+            # commands.append(['java', '-jar', tool_path, '/mnt/hdd1/zyang/Privacy-in-Code-Models/extract/utils.py'])
+            print(commands[-1])
+        
+        # launch the commands in parallel using a process pool
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            futures = [executor.submit(run_command, command) for command in commands]
+
+        # wait for all futures to complete and capture the output
+        outputs = [future.result() for future in futures]
+
+        # decode the output and error into strings (assuming utf-8 encoding)
+        output_strs = [output.decode("utf-8") for output, error in outputs]
+        error_strs = [error.decode("utf-8") for output, error in outputs]
+
+        # save output to files
+        for i, output_str in enumerate(output_strs):
+            with open(os.path.join(log_dir, "{}.log".format(i)), "w") as f:
+                f.write(output_str)
+
+        logger.info("Batch {}-{} finished".format(_start, _end))
