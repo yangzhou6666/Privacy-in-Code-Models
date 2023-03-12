aef75266939c99c713c9c731901708925a26443a
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2023-03-12 09:59:15 +0800

    chore(extract): remove unncessary models

--- extract/scripts_generator.py
+++ extract/scripts_generator.py
@@ -5,32 +5,16 @@ import os
 def run_merge(model, temp, len, k):
     cmd = 'python3 merge.py --model {} --temperature {} --seq_len {} --top_k {} &'.format(model, temp, len, k)
     print(cmd)
+    exit()
     os.system(cmd)
     
 
 if __name__ == '__main__':
     configs = [
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 256, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 768, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 1024, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 512, 'k': 10},
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 512, 'k': 20},
-        {'model': 'Salesforce/codegen-350M-mono', 'temp': 1.0, 'len': 512, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 256, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 768, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 1024, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 512, 'k': 5},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 512, 'k': 10},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 512, 'k': 20},
-        {'model': 'Salesforce/codegen-350M-multi', 'temp': 1.0, 'len': 512, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 256, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 768, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 1024, 'k': 40},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 512, 'k': 5},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 512, 'k': 10},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 512, 'k': 20},
-        {'model': 'Salesforce/codegen-350M-nl', 'temp': 1.0, 'len': 512, 'k': 40},
-
+        {'model': 'codeparrot/codeparrot', 'temp': 1.0, 'len': 1024, 'k': 5},
+        {'model': 'codeparrot/codeparrot', 'temp': 1.0, 'len': 1024, 'k': 10},
+        {'model': 'codeparrot/codeparrot', 'temp': 1.0, 'len': 1024, 'k': 20},
+        {'model': 'codeparrot/codeparrot', 'temp': 1.0, 'len': 1024, 'k': 40},
     ]
     
     for config in configs:
